59a30590e64da706af2cf935ffec75e2a27f8f1a
hoanglamnguyentb <hoanglamnguyentb@gmail.com>
2024-12-01 22:12:07 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { ClerkProvider } from '@clerk/nextjs';
+//Hello
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
